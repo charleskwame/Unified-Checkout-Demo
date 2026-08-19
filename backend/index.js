1c08afc3ca7dacb9bbac5403a3393af42d4b8478
Dev Charles <charlestettehnull@gmail.com>
2026-08-19 20:25:50 +0000

    cors made global

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const SHARED_SECRET = process.env.CYBERSOURCE_API_SECRET_KEY;
 const resourcePath = "/uc/v1/sessions";
 
 const app = express();
-app.use(cors({ origin: ["http://127.0.0.1:5500", "http://localhost:5500", "https://localhost:5500"] }));
+app.use(cors({ origin: ["*"] }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
