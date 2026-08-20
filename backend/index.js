aad9de513a15b3b71c41ac8c9483f76dea4fe119
Dev Charles <charlestettehnull@gmail.com>
2026-08-20 08:21:07 +0000

    allowed frontend origin in backend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const resourcePath = "/uc/v1/sessions";
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000"];
+const allowedOrigins = ["https://unified-checkout-frontend.vercel.app/"];
 
 app.use(cors());
 
