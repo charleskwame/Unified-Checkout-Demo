25251b159a08c418190a1c0c889e101d521809e3
Dev Charles <charlestettehnull@gmail.com>
2026-08-20 12:55:51 +0000

    Removed Redundant Files

--- backend/routes/session.route.js
+++
@@ -1,6 +0,0 @@
-const Route = require("express").Router();
-const { createCheckoutSession } = require("../controllers/session.controller.js");
-
-const sessionRoute = Route.post("/checkout-session", createCheckoutSession);
-
-module.exports = sessionRoute;
