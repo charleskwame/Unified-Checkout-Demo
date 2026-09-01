40245c21d4d95390a9445430e54da5db5cd8d1bf
Dev Charles <charlestettehnull@gmail.com>
2026-09-01 13:15:02 +0000

    replacing payment completed with mounted

--- frontend/index.js
+++ frontend/index.js
@@ -107,7 +107,7 @@ const startWithVAS = async (captureContext) => {
       console.log("Checkout ready:", paymentData);
     });
 
-    checkout.on("paymentCompleted", async (eventData) => {
+    checkout.on("mounted", async (eventData) => {
       try {
         const transientToken = eventData?.transientToken || eventData?.token;
 
