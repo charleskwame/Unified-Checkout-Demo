39e29d3d971c33aa8187136c0c9f8bcedc6d0584
Dev Charles <charlestettehnull@gmail.com>
2026-09-22 12:00:40 +0000

    enabled cors

--- recurring_billing_implementation/backend/index.js
+++ recurring_billing_implementation/backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const { createHeaders } = require("cybersource-auth");
