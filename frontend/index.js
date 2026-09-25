f856caea490de3d02af9797e7ca0f011cf3566b4
Dev Charles <charlestettehnull@gmail.com>
2026-09-25 16:12:02 +0000

    testing in live

--- frontend/index.js
+++ frontend/index.js
@@ -6,9 +6,9 @@ const paymentPayload = {
   clientVersion: "1.0",
   country: "US",
   locale: "en_US",
-  completeMandate: {
-    type: "CAPTURE",
-  },
+  // completeMandate: {
+  //   type: "CAPTURE",
+  // },
   data: {
     orderInformation: {
       amountDetails: {
@@ -106,17 +106,19 @@ const startWithVAS = async (captureContext) => {
       });
 
       console.log("Payment result response:", response);
-      return;
+      // return;
 
-      if (response.data?.decoded?.status === "AUTHORIZED") {
-        alert("Your payment was successful. Your payment id is: " + response.data.decoded.id + " This is a test transaction. Thank you");
-      }
+      // if (response.data?.decoded?.status === "AUTHORIZED") {
+      //   alert("Your payment was successful. Your payment id is: " + response.data.decoded.id + " This is a test transaction. Thank you");
+      // }
 
-      if (response.status === 200) {
-        console.log(response);
-      } else {
-        console.log("Payment Processing Failed");
-      }
+      // if (response.status === 200) {
+      //   console.log(response);
+      // } else {
+      //   console.log("Payment Processing Failed");
+      // }
+
+      console.log("Subscription result response:", response.data);
     } else {
       throw new Error("Unified Checkout returned no payment result.");
     }
